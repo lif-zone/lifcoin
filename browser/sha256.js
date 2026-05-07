43aec70335c77292327e76bdb883a86c66724be7
Bright Life <bright@bright.center>
2026-05-08 02:37:24 +0300

    update

--- browser/sha256.js
+++ browser/sha256.js
@@ -13,8 +13,8 @@
  *   https://github.com/indutny/hash.js/blob/master/lib/hash/sha/256.js
  */
 'use strict';
-const assert = globalThis.assert || $lif.assert;
-const Buffer = globalThis.Buffer || $lif.Buffer;
+const assert = globalThis.assert || globalThis.$lif.assert;
+const Buffer = globalThis.Buffer || globalThis.$lif.Buffer;
 
 /*
  * Constants
