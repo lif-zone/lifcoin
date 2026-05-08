3439aa6774a9434807e169106ba406a674a5c38f
Bright Life <bright@bright.center>
2026-05-09 01:10:37 +0300

    update

--- browser/mine_worker_init.js
+++ browser/mine_worker_init.js
@@ -1,3 +1,8 @@
 import './node_env.js';
+// Chrome 148 Workers in Chrome 148 still have a bug of incorrect loading
+// order of sub-module static import, which gets modules that need Buffer
+// to be loaded before Buffer is added to global scope. To meantime solve it
+// dynamic import() is used.
+//import './mine_worker.js';
 await import('./mine_worker.js');
 
