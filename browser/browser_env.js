70f72e3873f032bd0c2f50385c8d4c7da7ca9427
Bright Life <bright@bright.center>
2026-05-14 23:53:37 +0300

    update

--- browser/browser_env.js
+++ browser/browser_env.js
@@ -1,4 +1,5 @@
 // LICENSE_CODE JPL browser compat layer for node.js
+/*global process*/
 let browser_env = process.browser_env ||= {};
 import {EventEmitter} from 'events';
 browser_env.EventEmitter = EventEmitter;
